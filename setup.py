5b33c88f30237875ece0de4898c9987f41770f17
اکسیر <134607898+exir-exchange@users.noreply.github.com>
2023-05-27 13:46:16 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='hollaex-py-lib',
+    name='exir-py-lib',
     version='1',
     license='MIT',
     author="Mahdi",
     author_email='mahdi@hollaex.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    url='https://github.com/hollaex/hollaex-py-lib',
-    keywords='Hollaex',
+    url='https://github.com/exir-exchange/exir-py-lib',
+    keywords='Exir',
     install_requires=[
           
       ],
